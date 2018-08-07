d778a840b2f7d61255ff9ab5386f56af779cf461
Baha Ganyev <ganyev7@gmail.com>
2018-08-07 20:56:21 +0600

    created SubcategoriesViewController

---
+++ Courses/ViewControllers/SubcategoriesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SubcategoriesViewController.swift
+//  Courses
+//
+//  Created by Baha Ganyev on 07.08.2018.
+//  Copyright © 2018 Baha Ganyev. All rights reserved.
+//
+
+import UIKit
+
+class SubcategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
