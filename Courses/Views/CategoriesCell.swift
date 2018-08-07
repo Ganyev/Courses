a26e06b8d20bea7c9b5ac0d61a9c939a39f9ae94
Baha Ganyev <ganyev7@gmail.com>
2018-08-07 20:31:47 +0600

    создал CategoriesViewController

---
+++ Courses/Views/CategoriesCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoriesCell.swift
+//  Courses
+//
+//  Created by Baha Ganyev on 07.08.2018.
+//  Copyright © 2018 Baha Ganyev. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesCell: UICollectionViewCell {
+    
+}
