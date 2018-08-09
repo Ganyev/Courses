17c9d0b0b1703942b4b2761494d0fbbe0a6bb027
Baha Ganyev <ganyev7@gmail.com>
2018-08-09 08:27:31 +0600

    added CoursesListViewController

---
+++ Courses/ViewControllers/CoursesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CoursesListViewController.swift
+//  Courses
+//
+//  Created by Baha Ganyev on 09.08.2018.
+//  Copyright © 2018 Baha Ganyev. All rights reserved.
+//
+
+import UIKit
+
+class CoursesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
